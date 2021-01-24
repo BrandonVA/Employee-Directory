f3742d5ec171ddbccf3382eb6b801101f31d799d
BrandonVA <codebva@gmail.com>
2021-01-24 12:01:37 -0800

    Fixed sortByEmployee function to actually work

--- src/components/EmployeeDirectory.js
+++ src/components/EmployeeDirectory.js
@@ -2,19 +2,16 @@ import React, { Component } from "react";
 import EmployeeList from "./EmployeeList";
 import API from "../utils/API";
 
+import Test from "./Test";
+
 class EmployeeDirectory extends Component {
   state = {
     result: {},
+    sortedBy: "",
   };
   componentDidMount() {
     this.getEmployees();
   }
-
-  //   getEmployees = () => {
-  //     API.search()
-  //       .then((res) => this.setState({ result: res }))
-  //       .catch((err) => console.log(err));
-  //   };
 
   getEmployees = () => {
     API.search()
@@ -23,13 +20,30 @@ class EmployeeDirectory extends Component {
   };
 
   sortByName = () => {
-    const sortedEmployees = this.state.result.sort((person) => person.name);
-    return sortedEmployees;
+    // const sort_az = (a, b) =>
+    //   a.name.first.toLowerCase() > b.name.first.toLowerCase() ? 1 : -1;
+    // const sort_za = (a, b) =>
+    //   a.name.first.toLowerCase() < b.name.first.toLowerCase() ? 1 : -1;
+
+    if (this.state.sortedBy === "name_a-z") {
+      const sortedEmployees = this.state.result.sort((a, b) =>
+        a.name.first.toLowerCase() < b.name.first.toLowerCase() ? 1 : -1
+      );
+      this.setState({ result: sortedEmployees, sortedBy: "name_z-a" });
+    } else {
+      const sortedEmployees = this.state.result.sort((a, b) =>
+        a.name.first.toLowerCase() > b.name.first.toLowerCase() ? 1 : -1
+      );
+      this.setState({ result: sortedEmployees, sortedBy: "name_a-z" });
+    }
+
+    console.log(this.state.sortedBy);
   };
 
   render() {
     return (
       <div>
+        <Test sort={this.sortByName} />
         {this.state.result.length > 0 ? (
           <EmployeeList
             employees={this.state.result}
