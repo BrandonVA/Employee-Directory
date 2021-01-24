22189effbfd560aaecd42a5e5076f994bb4f758e
BrandonVA <codebva@gmail.com>
2021-01-23 22:43:23 -0800

    added map function to loop through employees and render out an li.

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-const EmployeeList = () => {
-    return (
-        <div>
-            <h1>Hello World I'm Learning React!!!</h1>
-        </div>
-    );
+const EmployeeList = (props) => {
+  console.log(props.employees);
+  //   const listOf;
+  return (
+    <div>
+      <h1>Hello World I'm Learning React!!!</h1>
+      <ul>
+        {props.employees.map((employee, index) => (
+          <li key={index}>{employee.name.first}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default EmployeeList;
