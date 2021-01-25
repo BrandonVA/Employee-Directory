77fc67d40d4d8d685c33d9fb5887ceba4895aad5
BrandonVA <codebva@gmail.com>
2021-01-25 12:33:42 -0800

    Added bootstrap row and cols

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -2,7 +2,7 @@ import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 
 const EmployeeList = (props) => {
-  //   console.log(props.employees.name.first);
+  console.log(props.employees);
   //   props.employees.forEach((item) => console.log(item.name.first));
   //   const listOf;
   return (
@@ -10,7 +10,25 @@ const EmployeeList = (props) => {
       <ListGroup as="ul">
         {props.employees.map((employee, index) => (
           <ListGroup.Item as="li" key={index}>
-            {employee.name.first}
+            <div className="row">
+              <div className="col-3">
+                <img
+                  src={employee.picture.thumbnail}
+                  alt={`of ${employee.name.first}`}
+                />
+              </div>
+              <div className="col-3">
+                <p>
+                  {employee.name.first} {employee.name.last}
+                </p>
+              </div>
+              <div className="col-3">
+                <p>{employee.cell}</p>
+              </div>
+              <div className="col-3">
+                <p>{employee.email}</p>
+              </div>
+            </div>
           </ListGroup.Item>
         ))}
       </ListGroup>
