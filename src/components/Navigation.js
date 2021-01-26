eb2f4bb796ce9aef589ba25d7f3d4cdcebe68a39
BrandonVA <codebva@gmail.com>
2021-01-26 13:05:02 -0800

    Added select element with values for how to filter employees by

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -11,7 +11,28 @@ const Navigation = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Form inline className="ml-auto">
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Form.Group controlId="filterBy">
+            <Form.Label className="mr-1">Filter By:</Form.Label>
+            <Form.Control as="select" custom>
+              <option value="all">All</option>
+              <option value="name">Name</option>
+              <option value="phone">Phone</option>
+              <option value="email">Email</option>
+            </Form.Control>
+          </Form.Group>
+          <FormControl
+            id="filterForInput"
+            type="text"
+            placeholder="Search"
+            className="mr-sm-2"
+            onChange={() => {
+              var selectVal = document.getElementById("filterBy").value;
+              var inputVal = document.getElementById("filterForInput").value;
+              //   const test = formData.getAll();
+              console.log(selectVal);
+              console.log(inputVal);
+            }}
+          />
           <Button variant="outline-success">Search</Button>
         </Form>
       </Navbar.Collapse>
