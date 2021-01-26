02285bed74da81fda8b3b74830cc7b12eeef0c99
BrandonVA <codebva@gmail.com>
2021-01-26 14:28:27 -0800

    Added a filter function to filter the employees based on phone number, name, and email.

--- src/components/EmployeeDirectory.js
+++ src/components/EmployeeDirectory.js
@@ -9,6 +9,7 @@ class EmployeeDirectory extends Component {
   state = {
     result: {},
     sortedBy: "",
+    cachedResult: "",
   };
   componentDidMount() {
     this.getEmployees();
@@ -16,12 +17,17 @@ class EmployeeDirectory extends Component {
 
   getEmployees = () => {
     API.search()
-      .then((res) => this.setState({ result: res.data.results }))
+      .then((res) =>
+        this.setState({
+          result: res.data.results,
+          cachedResult: res.data.results,
+        })
+      )
       .catch((err) => console.log(err));
   };
 
   sortBy_id = (event) => {
-    console.log(event.target.id);
+    // console.log(event.target.id);
     sortEmployees(
       this.state.sortedBy,
       event.target.id,
@@ -33,15 +39,42 @@ class EmployeeDirectory extends Component {
         });
       }
     );
-    console.log(this.state.sortedBy);
+    // console.log(this.state.sortedBy);
   };
 
-  filterEmployees = () => {};
+  filterEmployees = () => {
+    const selectVal = document.getElementById("filterBy").value;
+    const inputVal = document.getElementById("filterForInput").value;
+    const newArray = this.state.cachedResult.filter((item) => {
+      let fullName = `${item.name.first.toLowerCase()} ${item.name.last.toLowerCase()}`;
+      let name_test = fullName.includes(inputVal.toLowerCase());
+      let email_test = item.email.includes(inputVal.toLowerCase());
+      let cell_test = item.cell.includes(inputVal);
+      switch (selectVal) {
+        case "name":
+          return name_test;
+        case "email":
+          return email_test;
+        case "phone":
+          return cell_test;
+        default:
+          return name_test || email_test || cell_test;
+      }
+    });
+
+    this.setState({
+      result: newArray,
+    });
+
+    // console.log(selectVal);
+    // console.log(inputVal);
+    // console.log(newArray);
+  };
 
   render() {
     return (
       <div>
-        <Navigation />
+        <Navigation filter={this.filterEmployees} />
         <EmployeeNav sort={this.sortBy_id} />
         {this.state.result.length > 0 ? (
           <EmployeeList
