c54518bc6bfb8a5fd71d14af9bb595652dd6b334
BrandonVA <codebva@gmail.com>
2021-01-27 10:26:21 -0800

    Cleaned up Code.

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -2,9 +2,6 @@ import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 
 const EmployeeList = (props) => {
-  //   console.log(props.employees);
-  //   props.employees.forEach((item) => console.log(item.name.first));
-  //   const listOf;
   return (
     <div>
       <ListGroup as="ul">
