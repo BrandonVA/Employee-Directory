9b8d766c902bd712512712dbf988ca7a90eb2258
BrandonVA <codebva@gmail.com>
2021-01-27 16:44:57 -0800

    Changed layout and how the content is displayed on breakpoints.

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -8,22 +8,31 @@ const EmployeeList = (props) => {
         {props.employees.map((employee, index) => (
           <ListGroup.Item as="li" key={index}>
             <div className="row">
-              <div className="col-3">
-                <img
-                  src={employee.picture.thumbnail}
-                  alt={`of ${employee.name.first}`}
-                />
+              <div className="col-6">
+                <div className="row">
+                  <div className="col-lg-6">
+                    <img
+                      src={employee.picture.thumbnail}
+                      alt={`of ${employee.name.first}`}
+                    />
+                  </div>
+                  <div className="col-lg-6">
+                    <p>
+                      {employee.name.first} {employee.name.last}
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div className="col-3">
-                <p>
-                  {employee.name.first} {employee.name.last}
-                </p>
-              </div>
-              <div className="col-3">
-                <p>{employee.cell}</p>
-              </div>
-              <div className="col-3">
-                <p>{employee.email}</p>
+
+              <div className="col-6">
+                <div className="row">
+                  <div className="col-lg-6">
+                    <p>{employee.cell}</p>
+                  </div>
+                  <div className="col-lg-6">
+                    <p>{employee.email}</p>
+                  </div>
+                </div>
               </div>
             </div>
           </ListGroup.Item>
