fb9b1fb19888502781d22524fd8ebff2444c336a
BrandonVA <codebva@gmail.com>
2021-01-27 16:45:53 -0800

    Added some small adjustments to the break points and changes the class names depending on screenwidth.

--- src/components/EmployeeNav.js
+++ src/components/EmployeeNav.js
@@ -1,33 +1,32 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
 
-function EmployeeNav({ sort }) {
-  // console.log(sort);
+function EmployeeNav({ sort, screenWidth }) {
   return (
     <Nav variant="tabs" defaultActiveKey="/home">
       <div style={{ width: "100%" }} className="row">
-        <div className="col-3">
+        <div className={parseInt(screenWidth) < 768 ? "col-4" : "col-3"}>
           <Nav.Item>
             <Nav.Link eventKey="disabled" disabled>
               Sort by:
             </Nav.Link>
           </Nav.Item>
         </div>
-        <div className="col-3">
+        <div className={parseInt(screenWidth) < 768 ? "col-4" : "col-3"}>
           <Nav.Item>
             <Nav.Link eventKey="link-1" onClick={sort} id="name">
               Name
             </Nav.Link>
           </Nav.Item>
         </div>
-        <div className="col-3">
+        <div className={parseInt(screenWidth) < 768 ? "d-none" : "col-3"}>
           <Nav.Item>
             <Nav.Link eventKey="link-3" onClick={sort} id="phone">
               Phone Number
             </Nav.Link>
           </Nav.Item>
         </div>
-        <div className="col-3">
+        <div className={parseInt(screenWidth) < 768 ? "col-4" : "col-3"}>
           <Nav.Item>
             <Nav.Link eventKey="link-2" onClick={sort} id="email">
               Email
